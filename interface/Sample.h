da4b4817e2128e2eb55515975de333d740175cd4
martinavit <martina.vit@cern.ch>
2019-04-19 14:33:46 +0200

    Create Sample.h

---
+++ interface/Sample.h
@@ -0,0 +1,71 @@
+/*
+Class used for storing the info related to a particular sample: xSec, process name, file name
+*/
+#ifndef Sample_H
+#define Sample_H
+
+//include c++ library classes
+#include <string>
+#include <vector>
+#include <iostream>
+#include <sstream>
+#include <memory>
+
+//include ROOT classes
+#include "TFile.h"
+
+class Sample{
+    friend std::ostream& operator<<( std::ostream& os, const Sample& ); 
+    public:
+        Sample() = default;
+        //Sample(const std::string& file, const std::string& proc, double cross = 0);
+        Sample( const std::string& line, const std::string& directory ); 
+        Sample( std::istream&, const std::string& directory ); 
+
+        std::string getFileName() const { return fileName; }
+
+        std::string getProcessName() const { return process; } 
+    
+        //to prevent overlapping file names when re-using a sample in both the 2016 and 2017 data lists 
+        std::string getUniqueName() const { return uniqueName; }
+
+        double getXSec() const { return xSec; }
+
+        bool isData() const { return isDataSample; }
+
+        bool isMC() const { return !isDataSample; }
+
+        bool is2017() const { return is2017Sample; }
+
+        bool is2016() const { return !is2017Sample; }
+
+        bool isSMSignal() const { return smSignal; }
+
+        bool isNewPhysicsSignal() const { return newPhysicsSignal; }
+
+        std::shared_ptr<TFile> getFile() const;
+
+    private:
+        void setData(); 
+
+        void set2017();
+
+        void setOptions(const std::string&);
+
+        std::string fileName;
+        std::string process;
+        std::string uniqueName;
+        std::string directory;
+
+        double xSec;
+        bool isDataSample;
+        bool is2017Sample;
+        bool smSignal;
+        bool newPhysicsSignal;
+
+};
+
+//read a txt file containing a list of samples
+std::vector< Sample > readSampleList( const std::string& list, const std::string& directory );
+        
+#endif
