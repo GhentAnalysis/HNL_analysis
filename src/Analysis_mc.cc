61c468839ec5e7e5019703c77aa05d52e83fb139
martina.vit <martina.vit@cern.ch>
2019-05-09 13:25:15 +0200

    pippo_9maggio

--- src/Analysis_mc.cc
+++ src/Analysis_mc.cc
@@ -1079,13 +1079,13 @@ void Analysis_mc::analisi( unsigned jaar, const std::string& list, const std::st
 
 	if (cha ==0 || cha ==1 ||cha ==2 ||cha == 6) plotDataVSMC_mu(cat,cha,dist,
 								     dataYields[dist][cha][cat], bkgYields[dist][cha][cat],
-								     eff_names,nSamples_eff -  nSamples_signal_mu,
+								     eff_names,nSamples_eff -  nSamples_signal_mu - 10,
 								     catNames[cat], channelNames[cha], channelNames[cha]+"_"+ Histnames_ossf[dist]+"_"+catNames[cat],
 								     true,
 								     2, true, signals_mu,  sigNames_mu , nSamples_signal_mu, false);
 	if (cha ==3 || cha ==4 ||cha ==5 ||cha == 7) plotDataVSMC_e (cat,cha,dist,
 								     dataYields[dist][cha][cat], bkgYields[dist][cha][cat],
-								     eff_names,nSamples_eff -  nSamples_signal_mu,
+								     eff_names,nSamples_eff -  nSamples_signal_mu - 10,
 								     catNames[cat], channelNames[cha],channelNames[cha]+"_"+ Histnames_ossf[dist]+"_"+catNames[cat],
 								     true,
 								     2, true, signals_e,  sigNames_e , nSamples_signal_e, false);
