5a4783f5909597ec9f114691edf2cd359c5ec7b3
martina.vit <martina.vit@cern.ch>
2019-05-14 18:33:32 +0200

    14maggio

--- src/Analysis_mc.cc
+++ src/Analysis_mc.cc
@@ -732,7 +732,7 @@ void Analysis_mc::analisi( unsigned jaar, const std::string& list, const std::st
       displacedC=0;
       for(unsigned l = 0; l < lCount; ++l){
 	for(unsigned j = l+1; j < lCount; ++j){
-	  if (_eventNb==96541 || _eventNb==113885 || _eventNb==134456 || _eventNb==136224 ) std::cout<<"inside loop leptons displaced"<<"  "<<_eventNb<<"   "<<_lPt[ind[l]]<<std::endl;
+	  if (_eventNb==96541 || _eventNb==113885 || _eventNb==134456 || _eventNb==136224 ) std::cout<<"inside loop leptons displaced"<<"  "<<_eventNb<<"  pt  "<<_lPt[ind[l]]<<"   "<<<<_lPt[ind[j]]<<std::endl;
 	  if(!lepIsDisplaced(ind[l] , ind_new_leading, ind)) continue;
 	  if (_eventNb==96541 || _eventNb==113885 || _eventNb==134456 || _eventNb==136224 ) std::cout<<"l is dispalced"<<"  "<<_eventNb<<_lPt[ind[l]]<<std::endl;
 
