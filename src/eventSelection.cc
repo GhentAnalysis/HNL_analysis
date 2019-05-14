1cd9ccb4b4c7c6ddde790241073ef271240d2612
martina.vit <martina.vit@cern.ch>
2019-05-14 19:02:42 +0200

    14maggio

--- src/eventSelection.cc
+++ src/eventSelection.cc
@@ -73,33 +73,34 @@ int Analysis_mc::l1Index(const std::vector<unsigned>& ind){
 //______________________________________________check if they can be called displaced
 bool Analysis_mc::lepIsDisplaced(const unsigned leptonIndex, int index_taken_by_l1, std::vector<unsigned>& ind) const{
   int number_found_verteces=-1;
-  std::cout<<"in lepIsdisplaced:"<<std::endl;
+  std::cout<<"      in lepIsdisplaced:"<<std::endl;
   if (leptonIndex == index_taken_by_l1) return false;
-  std::cout<<"no leading"<<std::endl;
+  std::cout<<"      no leading"<<std::endl;
 
   if (!lepIsFOBase(leptonIndex)) return false;
-  std::cout<<"si FO"<<std::endl;
+  std::cout<<"      si FO"<<std::endl;
 
   if (fabs(_dxy[leptonIndex]) < dxy_cut) return false;
-    std::cout<<"si dxy cut"<<std::endl;
+    std::cout<<"      si dxy cut"<<std::endl;
 
   //looking for a common vertex with an other lepton
   for(unsigned sd = 0; sd < ind.size(); ++sd){
-    std::cout<<"--> in lepIsdisplaced:"<<std::endl;
+    std::cout<<"            --> in lepIsdisplaced:"<<std::endl;
 
     if (leptonIndex == ind[sd]) continue;
-    std::cout<<"--> no the same:"<<std::endl;
+    std::cout<<"            --> no the same:"<<std::endl;
 
     if (ind[sd] == index_taken_by_l1) continue;
-        std::cout<<"--> noleading"<<std::endl;
+        std::cout<<"            --> noleading"<<std::endl;
 
     if (fabs(_dxy[ind[sd]]) < dxy_cut) continue;
-        std::cout<<"--> si displaced"<<std::endl;
+        std::cout<<"            --> si displaced"<<std::endl;
 
     if (!lepIsFOBase(ind[sd])) continue;
-        std::cout<<"--> si FO"<<std::endl;
+        std::cout<<"            --> si FO"<<std::endl;
 
     for(unsigned v = 0; v < _nVFit; ++v){
+      std::cout<<v<<": "<< _vertices[v][0]<<"               what is supposed to be: "<< leptonIndex<<"  "<<ind[sd]<<std::endl;
       if (vertex_found(leptonIndex,ind[sd],  _vertices[v][0]) ) ++number_found_verteces;   
     }//loop vertecies
   }//loop second lepton
