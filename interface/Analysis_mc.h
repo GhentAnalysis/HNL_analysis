c87da5fbbc853381fe481c7f1e14aed3327b3ebe
martinavit <martina.vit@cern.ch>
2019-06-07 17:20:05 +0200

    isOnlyMC

--- interface/Analysis_mc.h
+++ interface/Analysis_mc.h
@@ -776,7 +776,8 @@ class Analysis_mc : public TObject {
   unsigned year;
 
   bool isCRRun=false;
-  bool isSRRun=true;
+  bool isSRRun=false;
+  bool isOnlyMC=true;	
 
   TTree* fChain;                                                          //current Tree
   std::shared_ptr<TFile> sampleFile;                                      //current sample
