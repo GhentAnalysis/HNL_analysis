27cbc564c898a7800f279b397f9f76cfdb758bfe
martinavit <martina.vit@cern.ch>
2019-08-29 16:16:47 +0200

    Update Reweighter.h

--- interface/Reweighter.h
+++ interface/Reweighter.h
@@ -15,7 +15,7 @@
 //Class storing scale-factor weights to be used in events
 class Reweighter{
     public:
-        Reweighter(const std::vector<Sample>&, const bool is2016, const std::string& bTagWorkingPoint = "medium");
+        Reweighter(const std::vector<Sample>&, const bool is2016);
         ~Reweighter();
 
         //pileup weight
@@ -27,22 +27,10 @@ class Reweighter{
 
         //pu weights (one for every sample)
         std::map< std::string, std::vector< std::shared_ptr<TH1D> > > puWeights;
-
-        //initialize all weight histograms
-        void initialize2016Weights();
-        void initialize2017Weights();
-
-        //return jet flavor index 1 -> 0, 4 -> 1, 5 -> 2
-        unsigned flavorInd(const unsigned jetFlavor) const{ 
-            return 0 + (jetFlavor == 4) + 2*(jetFlavor == 5);
-        }
-
-   
         //read pu weights for a given list of samples
         void initializePuWeights(const std::vector< Sample >&); 
     
         //initialize all weights 
-        void initializeAllWeights(const std::vector< Sample>&);
 
    
 };
