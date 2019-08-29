df350824749ef0e5d85086306cc01f27bfb9a578
martinavit <martina.vit@cern.ch>
2019-08-29 14:48:41 +0200

    pu reweighting

---
+++ src/Reweighter.cc
@@ -0,0 +1,99 @@
+#include "../interface/Reweighter.h"
+
+//include c++ library classes
+
+//include ROOT classes
+#include "TFile.h"
+#include "TROOT.h"
+
+//include other parts of code 
+#include "../interface/analysisTools.h"
+
+Reweighter::Reweighter(const std::vector<Sample>& samples, const bool sampleIs2016, const std::string& bTagWorkingPoint) : is2016(sampleIs2016), bTagWP( bTagWorkingPoint ) {
+    initializeAllWeights(samples);
+}
+
+void Reweighter::initializeAllWeights(const std::vector<Sample>& samples){
+
+    //initialize pu weights
+    initializePuWeights(samples);
+
+    //initialize b-tag weights
+    initializeBTagWeights();
+
+    //initialize electron weights 
+    initializeElectronWeights();
+
+    //initialize muon weights 
+    initializeMuonWeights();
+
+    //initialize fake-rate
+    initializeFakeRate();
+
+    //initialize prefiring probabilities
+    initializePrefiringProbabilities();
+}
+
+void Reweighter::initializePuWeights(const std::vector< Sample >& sampleList){
+
+    static const std::string minBiasVariation[3] = {"central", "down", "up"};
+    for(auto& sample : sampleList){
+
+        //no pu weights for data 
+        if( sample.isData() ) continue;
+
+        //open root file corresponding to sample
+        TFile* puFile = TFile::Open( (const TString&) "weights/pileUpWeights/puWeights_" + sample.getFileName() );
+
+        //extract pu weights 
+        for(unsigned var = 0; var < 3; ++var){
+            std::string histName = "puw_Run";
+            histName += (sample.is2016() ? "2016" : "2017");
+            histName += "Inclusive_" + minBiasVariation[var];
+            puWeights[sample.getUniqueName()].push_back( std::shared_ptr<TH1D> ( (TH1D*)puFile->Get( (const TString&) histName ) ) );
+
+            //make sure histogram does not get deleted when closing file
+            puWeights[sample.getUniqueName()].back()->SetDirectory(gROOT);
+        }
+        puFile->Close();
+    }
+}
+
+Reweighter::~Reweighter(){}
+
+double Reweighter::puWeight(const double nTrueInt, const Sample& sample, const unsigned unc) const{
+    if(unc < 3){
+
+        //find weights for given sample
+        const auto& weightVectorIter = puWeights.find(sample.getUniqueName() );
+
+        //check if pu weights are available for this sample
+        if( weightVectorIter == puWeights.cend() ){
+            std::cerr << "Error: no pu weights found for sample : " << sample.getUniqueName() << " returning weight 0  " << std::endl;
+            return 0.;
+        }
+
+        //WARNING: 2016 samples might be used when making 2017 plots when no 2017 equivalent is available
+        // In this case is2016() will return false, but the pileup weights for the sample will only exist
+        // up to 50 interactions. So one needs to be sure to check for what campaign the sample was generated.
+        // When using a sample (potentially simulated for 2017) for 2016 data analysis, always limit the bin
+        // to 50 interactions which is the limit to which the data pileup profile was evaluated.
+        bool sampleIsSimulatedFor2016 =  ( sample.getFileName().find("Summer16") != std::string::npos );
+        double maxBin = ( (sampleIsSimulatedFor2016 || is2016) ?  49.5 : 99.5 );
+
+        TH1D* weights = ( (*weightVectorIter).second)[unc].get();
+
+        //buggy events in 2017 MC can have negative pileup! catch them 
+        if( nTrueInt >= 0){
+            return weights->GetBinContent(weights->FindBin(std::min(nTrueInt, maxBin) ) );
+        } else {
+            std::cerr << "Error: event with nTrueInt = " << nTrueInt << " , returning weight 9999." << std::endl;
+            return 9999.;
+        }
+    }
+    else {
+        std::cerr << "Error: invalid pu uncertainty requested: returning weight 0" << std::endl;
+        return 0.;
+    }
+}
+
