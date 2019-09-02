3e613e583ff519d0f1dba58a9f38305b567ce9a8
martinavit <martina.vit@cern.ch>
2019-09-02 11:57:06 +0200

    adding bkgYields_SRb just for crosscheck reasons

--- interface/Analysis_mc.h
+++ interface/Analysis_mc.h
@@ -1269,6 +1269,8 @@ class Analysis_mc : public TObject {
 	
   TH1D*	plots_SR[nCoupling][nSystematic][nVariation][nSamples_eff +1];
   double weight_SR[nCoupling][nSystematic][nVariation][nSamples_eff +1];
+  TH1D* bkgYields_SR[nCoupling][nSystematic][nVariation][nSamples_eff - nSamples_signal]; //change to nSamples_eff if sig is removed
+
   TH1D*	sum_expected_SR[nCoupling][nSystematic][nVariation];
   //TH1D*	sum_observed_SR[channel][nSystematic][nVariation];
 
