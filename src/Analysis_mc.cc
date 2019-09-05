e1100958a865057913ff9fb368437c6eda137212
martinavit <martina.vit@cern.ch>
2019-09-05 11:49:59 +0200

    Update Analysis_mc.cc

--- src/Analysis_mc.cc
+++ src/Analysis_mc.cc
@@ -1098,17 +1098,17 @@ void Analysis_mc::analisi( const std::string& list, const std::string& directory
       // l1   
 	if (!SR_selection  ) continue;  
       if (flavors_3l[0] == 0) continue;
-	    std::cout<<"----------------------------------"<<std::endl;
-	    std::cout<<"sr channel: "<<SR_channel<<std::endl;
+	//    std::cout<<"----------------------------------"<<std::endl;
+	//    std::cout<<"sr channel: "<<SR_channel<<std::endl;
 	// µ and e ID SF    
        if (SR_channel > 2 ) weight_SR[ ele_case][pEle_index][0][effsam] = SF_prompt_ele(*&sf_prompt_ele, l1); 
        if (SR_channel <= 2 ) weight_SR[ muon_case][pMuo_index][0][effsam] = SF_prompt_muon(*&sf_prompt_muon, l1);
 	// µ trigger SF    
        if (SR_channel <= 2 ) weight_SR[muon_case][trigger_index][0][effsam] = SF_trigger_muon(*&sf_trigger_muon, l1);
 	//eta??? boh... desapparessidos   
-     std::cout<<"weight_SR[ ele_case][pEle_index][0][effsam] . " << weight_SR[ ele_case][pEle_index][0][effsam]<<std::endl;
-     std::cout<<"weight_SR[ muon_case][pMuo_index][0][effsam] . " << weight_SR[ muon_case][pMuo_index][0][effsam]<<std::endl;
-     std::cout<<"weight_SR[muon_case][trigger_index][0][effsam] . " << weight_SR[muon_case][trigger_index][0][effsam]<<std::endl;
+   //  std::cout<<"weight_SR[ ele_case][pEle_index][0][effsam] . " << weight_SR[ ele_case][pEle_index][0][effsam]<<std::endl;
+    // std::cout<<"weight_SR[ muon_case][pMuo_index][0][effsam] . " << weight_SR[ muon_case][pMuo_index][0][effsam]<<std::endl;
+  //   std::cout<<"weight_SR[muon_case][trigger_index][0][effsam] . " << weight_SR[muon_case][trigger_index][0][effsam]<<std::endl;
 
     
 	    
@@ -1191,7 +1191,7 @@ void Analysis_mc::analisi( const std::string& list, const std::string& directory
 	  weight_SR[ele_case][pEle_index][1][effsam] = SF_prompt_ele(*&sf_prompt_ele, l1)-SF_prompt_ele_error(*&sf_prompt_ele, l1);	  
 	  weight_SR[ele_case][pEle_index][2][effsam] = SF_prompt_ele(*&sf_prompt_ele, l1)+SF_prompt_ele_error(*&sf_prompt_ele, l1);	    
 	} 
-  std::cout<<"up---->"<<std::endl;	    
+  /*std::cout<<"up---->"<<std::endl;	    
      std::cout<<"weight_SR[ ele_case][pEle_index][2][effsam] . " << weight_SR[ ele_case][pEle_index][2][effsam]<<std::endl;
      std::cout<<"weight_SR[ muon_case][pMuo_index][2][effsam] . " << weight_SR[ muon_case][pMuo_index][2][effsam]<<std::endl;
      std::cout<<"weight_SR[muon_case][trigger_index][2][effsam] . " << weight_SR[muon_case][trigger_index][2][effsam]<<std::endl;
@@ -1199,7 +1199,7 @@ std::cout<<"down---->"<<std::endl;
      std::cout<<"weight_SR[ ele_case][pEle_index][1][effsam] . " << weight_SR[ ele_case][pEle_index][1][effsam]<<std::endl;
      std::cout<<"weight_SR[ muon_case][pMuo_index][1][effsam] . " << weight_SR[ muon_case][pMuo_index][1][effsam]<<std::endl;
      std::cout<<"weight_SR[muon_case][trigger_index][1][effsam] . " << weight_SR[muon_case][trigger_index][1][effsam]<<std::endl;
-
+*/
       // ----> SYS Pile UP!
       if (!samples[sam].isData()){  
 	for (int w_loop =0; w_loop < nCoupling; w_loop++){
@@ -1279,10 +1279,10 @@ std::cout<<"down---->"<<std::endl;
 	  if (SR_channel > 2 ) central_total_weight_ele *= weight_SR[1][w_loop][0][effsam];	      
 	} 	        	
       } 
-std::cout<<""<<std::endl;
-std::cout<<"scale: "<<	  scal<<std::endl;  
-std::cout<<"central_total_weight_mu: "<< central_total_weight_mu<<std::endl;
-std::cout<<"central_total_weight_ele: "<< central_total_weight_ele<<std::endl;
+//std::cout<<""<<std::endl;
+//std::cout<<"scale: "<<	  scal<<std::endl;  
+//std::cout<<"central_total_weight_mu: "<< central_total_weight_mu<<std::endl;
+//std::cout<<"central_total_weight_ele: "<< central_total_weight_ele<<std::endl;
 
       // electron case --> eee eeµ eeµ	  
       if (SR_selection){ // only final fianl step 
@@ -1320,11 +1320,11 @@ std::cout<<"central_total_weight_ele: "<< central_total_weight_ele<<std::endl;
 	     }    
 		    
 		    
-	    std::cout<<"isys: "<<iSystematics<<" . "<< "iVariation "<< iVariation<<std::endl;
-	    std::cout<<"central_divided_by_sys_muon . -> "<< central_total_weight_mu<<" / "<<weight_SR[muon_case][iSystematics][0][effsam]<<" = "<<central_divided_by_sys_muon<<std::endl;
-	    std::cout<<"central_divided_by_sys_ele . -> "<< central_total_weight_ele<<" / "<<weight_SR[ele_case][iSystematics][0][effsam]<<" = "<< central_divided_by_sys_ele<<std::endl;	    
-	    std::cout<<"final weight mu . "<< central_divided_by_sys_muon*weight_SR[muon_case][iSystematics][iVariation][effsam]<<std::endl;
-	    std::cout<<"final weight ele . "<< central_divided_by_sys_ele*weight_SR[ele_case][iSystematics][iVariation][effsam]<<std::endl;
+	//    std::cout<<"isys: "<<iSystematics<<" . "<< "iVariation "<< iVariation<<std::endl;
+	//    std::cout<<"central_divided_by_sys_muon . -> "<< central_total_weight_mu<<" / "<<weight_SR[muon_case][iSystematics][0][effsam]<<" = "<<central_divided_by_sys_muon<<std::endl;
+	//    std::cout<<"central_divided_by_sys_ele . -> "<< central_total_weight_ele<<" / "<<weight_SR[ele_case][iSystematics][0][effsam]<<" = "<< central_divided_by_sys_ele<<std::endl;	    
+	//    std::cout<<"final weight mu . "<< central_divided_by_sys_muon*weight_SR[muon_case][iSystematics][iVariation][effsam]<<std::endl;
+	//    std::cout<<"final weight ele . "<< central_divided_by_sys_ele*weight_SR[ele_case][iSystematics][iVariation][effsam]<<std::endl;
 	     
 	    if (iSystematics==pEle_index && central_divided_by_sys_muon*weight_SR[muon_case][iSystematics][iVariation][effsam] != central_total_weight_mu)std::cout<<"<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<"<<std::endl;
 	    }//end loop up-down		
