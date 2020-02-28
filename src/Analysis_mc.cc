c083e6d5dc75b285e8b2360194e1026400fb0103
martinavit <martina.vit@cern.ch>
2020-02-28 12:08:56 +0100

    Update Analysis_mc.cc

--- src/Analysis_mc.cc
+++ src/Analysis_mc.cc
@@ -2655,10 +2655,10 @@ void Analysis_mc::put_at_zero(TH1D *histo){
       error_original = histo-> GetBinError(i+1);
       error_to_add = histo-> GetBinContent(i+1);
       error_final=TMath::Sqrt(error_original*error_original   +    error_to_add*error_to_add );
-      histo-> SetBinContent(i+1, 0.00001);
+      histo-> SetBinContent(i+1, 0.00002);
       histo-> SetBinError(i+1, error_final);
-      if (error_final == 0) histo-> SetBinError(i+1,  0.00001);
-      if (error_final < 0.000001) histo-> SetBinError(i+1,  0.00001);
+      if (error_final == 0) histo-> SetBinError(i+1,  0.000002);
+      if (error_final < 0.000001) histo-> SetBinError(i+1,  0.000002);
 
     }
   }
