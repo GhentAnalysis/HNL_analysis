4fe936e599c4dc8a3e80f52e4c6e464dabad1b8d
martinavit <martina.vit@cern.ch>
2020-06-25 14:40:42 +0200

    put all bin at 0 ± 0

--- src/analysisTool.cc
+++ src/analysisTool.cc
@@ -910,8 +910,8 @@ void Analysis_mc::put_at_zero(int iSystematics,int iVariation,int channel, int o
 	error_original = histo-> GetBinError(i+1);
 	error_to_add = histo-> GetBinContent(i+1);
 	error_final=TMath::Sqrt(error_original*error_original   +    error_to_add*error_to_add );
-	histo-> SetBinContent(i+1, 0.);
-	histo-> SetBinError(i+1, error_final);
+	histo-> SetBinContent(i+1, 0.0);
+	histo-> SetBinError(i+1, 0.0);
 	//if (error_final == 0) histo-> SetBinError(i+1,  0.);
 	//if (error_final < 0.000001) histo-> SetBinError(i+1,  0.);
       }
