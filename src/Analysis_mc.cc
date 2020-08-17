f42cd2cf1e08b57b9401d6b23287ee4dda731b7c
martina vit <martina.vit@cern.ch>
2020-08-17 14:53:32 +0200

    mll < 20

--- src/Analysis_mc.cc
+++ src/Analysis_mc.cc
@@ -1249,7 +1249,7 @@ void Analysis_mc::analisi( //const std::string& list, const std::string& directo
 	                 M_3L_combined < 80 && 
 	                 min_delta_phi > 1 &&
 	                 vtxRvtxPcosAlpha > 0.99  &&
-	                 M_l2l3_combined < 12 &&
+	                 M_l2l3_combined < 20 &&
 			 sum_vec_l2l3 > 15 &&
 	                 D2_delta_pv_svSig > 20 &&	
 	                 prob_vertex > 0.001 &&
@@ -1270,17 +1270,17 @@ void Analysis_mc::analisi( //const std::string& list, const std::string& directo
       sel_7 = sel_6 && vtxRvtxPcosAlpha > 0.99;
       sel_8 = sel_7 && prob_vertex > 0.001;
       sel_9 = sel_8 && D2_delta_pv_svSig > 20;
-      sel_10= sel_9 && M_l2l3_combined < 12;
+      sel_10= sel_9 && M_l2l3_combined < 20;
       _0 = SR_selection;
-      _1 = M_3L_combined>50 && M_3L_combined<80 && min_delta_phi>1 && vtxRvtxPcosAlpha>0.99 && M_l2l3_combined<12 && sum_vec_l2l3>15 && D2_delta_pv_svSig>20 && prob_vertex>0.001 &&bjet == 0 &&vetoes;
-      _2 = v4l2.DeltaR(v4l3)<1 && M_3L_combined>50 && M_3L_combined<80 && vtxRvtxPcosAlpha>0.99 && M_l2l3_combined<12 && sum_vec_l2l3>15 && D2_delta_pv_svSig>20 && prob_vertex>0.001 &&bjet == 0 &&vetoes;
-      _3 = v4l2.DeltaR(v4l3)<1 && M_3L_combined>50 && M_3L_combined<80 && min_delta_phi>1 && vtxRvtxPcosAlpha>0.99 && M_l2l3_combined<12 && sum_vec_l2l3>15 && D2_delta_pv_svSig>20 && prob_vertex>0.001 &&vetoes;
-      _4 = v4l2.DeltaR(v4l3)<1 && M_3L_combined>50 && M_3L_combined<80 && min_delta_phi>1 && vtxRvtxPcosAlpha>0.99 && M_l2l3_combined<12 && sum_vec_l2l3>15 && D2_delta_pv_svSig>20 && prob_vertex>0.001 &&bjet == 0;
-      _5 = v4l2.DeltaR(v4l3)<1 && min_delta_phi>1 && vtxRvtxPcosAlpha>0.99 && M_l2l3_combined<12 && sum_vec_l2l3>15 && D2_delta_pv_svSig>20 && prob_vertex>0.001 &&bjet == 0 &&vetoes;
-      _6 = v4l2.DeltaR(v4l3)<1 && M_3L_combined>50 && M_3L_combined<80 && min_delta_phi>1 && vtxRvtxPcosAlpha>0.99 && M_l2l3_combined<12 && D2_delta_pv_svSig>20 && prob_vertex>0.001 &&bjet == 0 &&vetoes;
-      _7 = v4l2.DeltaR(v4l3)<1 && M_3L_combined>50 && M_3L_combined<80 && min_delta_phi>1 && M_l2l3_combined<12 && sum_vec_l2l3>15 && D2_delta_pv_svSig>20 && prob_vertex>0.001 &&bjet == 0 &&vetoes;
-      _8 = v4l2.DeltaR(v4l3)<1 && M_3L_combined>50 && M_3L_combined<80 && min_delta_phi>1 && vtxRvtxPcosAlpha>0.99 && M_l2l3_combined<12 && sum_vec_l2l3>15 && D2_delta_pv_svSig>20 && bjet == 0 &&vetoes;
-      _9 = v4l2.DeltaR(v4l3)<1 && M_3L_combined>50 && M_3L_combined<80 && min_delta_phi>1 && vtxRvtxPcosAlpha>0.99 && M_l2l3_combined<12 && sum_vec_l2l3>15 && prob_vertex>0.001 &&bjet == 0 &&vetoes;
+      _1 = M_3L_combined>50 && M_3L_combined<80 && min_delta_phi>1 && vtxRvtxPcosAlpha>0.99 && M_l2l3_combined<20 && sum_vec_l2l3>15 && D2_delta_pv_svSig>20 && prob_vertex>0.001 &&bjet == 0 &&vetoes;
+      _2 = v4l2.DeltaR(v4l3)<1 && M_3L_combined>50 && M_3L_combined<80 && vtxRvtxPcosAlpha>0.99 && M_l2l3_combined<20 && sum_vec_l2l3>15 && D2_delta_pv_svSig>20 && prob_vertex>0.001 &&bjet == 0 &&vetoes;
+      _3 = v4l2.DeltaR(v4l3)<1 && M_3L_combined>50 && M_3L_combined<80 && min_delta_phi>1 && vtxRvtxPcosAlpha>0.99 && M_l2l3_combined<20 && sum_vec_l2l3>15 && D2_delta_pv_svSig>20 && prob_vertex>0.001 &&vetoes;
+      _4 = v4l2.DeltaR(v4l3)<1 && M_3L_combined>50 && M_3L_combined<80 && min_delta_phi>1 && vtxRvtxPcosAlpha>0.99 && M_l2l3_combined<20 && sum_vec_l2l3>15 && D2_delta_pv_svSig>20 && prob_vertex>0.001 &&bjet == 0;
+      _5 = v4l2.DeltaR(v4l3)<1 && min_delta_phi>1 && vtxRvtxPcosAlpha>0.99 && M_l2l3_combined<20 && sum_vec_l2l3>15 && D2_delta_pv_svSig>20 && prob_vertex>0.001 &&bjet == 0 &&vetoes;
+      _6 = v4l2.DeltaR(v4l3)<1 && M_3L_combined>50 && M_3L_combined<80 && min_delta_phi>1 && vtxRvtxPcosAlpha>0.99 && M_l2l3_combined<20 && D2_delta_pv_svSig>20 && prob_vertex>0.001 &&bjet == 0 &&vetoes;
+      _7 = v4l2.DeltaR(v4l3)<1 && M_3L_combined>50 && M_3L_combined<80 && min_delta_phi>1 && M_l2l3_combined<20 && sum_vec_l2l3>15 && D2_delta_pv_svSig>20 && prob_vertex>0.001 &&bjet == 0 &&vetoes;
+      _8 = v4l2.DeltaR(v4l3)<1 && M_3L_combined>50 && M_3L_combined<80 && min_delta_phi>1 && vtxRvtxPcosAlpha>0.99 && M_l2l3_combined<20 && sum_vec_l2l3>15 && D2_delta_pv_svSig>20 && bjet == 0 &&vetoes;
+      _9 = v4l2.DeltaR(v4l3)<1 && M_3L_combined>50 && M_3L_combined<80 && min_delta_phi>1 && vtxRvtxPcosAlpha>0.99 && M_l2l3_combined<20 && sum_vec_l2l3>15 && prob_vertex>0.001 &&bjet == 0 &&vetoes;
       _10= v4l2.DeltaR(v4l3)<1 && M_3L_combined>50 && M_3L_combined<80 && min_delta_phi>1 && vtxRvtxPcosAlpha>0.99 && sum_vec_l2l3>15 && D2_delta_pv_svSig>20 && prob_vertex>0.001 &&bjet == 0 &&vetoes;
       //<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<     histogramm   <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
       double values[nDist] ={static_cast<double>(0) ,static_cast<double>(0) ,
