6a4cd1fff2f301ee6ef62b6507671ca5722bacb0
martina.vit <martina.vit@cern.ch>
2021-02-08 19:23:27 +0100

    all

--- src/Analysis_mc.cc
+++ src/Analysis_mc.cc
@@ -1727,7 +1727,7 @@ void Analysis_mc::analisi( //const std::string& list, const std::string& directo
       
       //D2_delta_pv_sv
       if (SR_channel <= 2) {
-	if (SR_selection   && bjet == 0){
+	//	if (SR_selection   && bjet == 0){
 	  if (SR_channel == 0 ) Histos2d[0][0][0][fill_2d] -> Fill(D2_delta_pv_sv, M_l2l3_combined,1);
 	  if (SR_channel == 0 ) Histos2d[1][0][0][fill_2d] -> Fill(D2_delta_pv_sv, M_l2l3_combined,1);
 	  if (SR_channel == 0 ) Histos2d[2][0][0][fill_2d] -> Fill(_vertex_X, _vertex_Y,1);
@@ -1762,7 +1762,7 @@ void Analysis_mc::analisi( //const std::string& list, const std::string& directo
             if (SR_channel == 2 ) Histos2d[7][0][2][fill_2d] -> Fill(_vertex_eta, _vertex_R,1);
             if (SR_channel == 2 ) Histos2d[9][0][2][fill_2d] -> Fill(_vertex_theta, _vertex_R,1);
 	  }
-	}
+	  //}
     
 	/*if (SR_selection  && bjet == 0 ){
 	  if (SR_channel == 0 ) Histos2d[0][1][0][fill] -> Fill(D2_delta_pv_sv, M_l2l3_combined,1);
@@ -1804,7 +1804,7 @@ void Analysis_mc::analisi( //const std::string& list, const std::string& directo
 
 
       if (SR_channel > 2) {
-	if (SR_selection   && bjet == 0){
+	//if (SR_selection   && bjet == 0){
 	  if (SR_channel == 3 ) Histos2d[0][0][3][fill_2d] -> Fill(D2_delta_pv_sv, M_l2l3_combined,1);
 	  if (SR_channel == 3 ) Histos2d[1][0][3][fill_2d] -> Fill(D2_delta_pv_sv, M_l2l3_combined,1);
 	  if (SR_channel == 3 ) Histos2d[2][0][3][fill_2d] -> Fill(_vertex_X, _vertex_Y,1);
@@ -1839,7 +1839,7 @@ void Analysis_mc::analisi( //const std::string& list, const std::string& directo
             if (SR_channel == 5 ) Histos2d[7][0][5][fill_2d] -> Fill(_vertex_eta, _vertex_R,1);
             if (SR_channel == 5 ) Histos2d[9][0][5][fill_2d] -> Fill(_vertex_theta, _vertex_R,1);
 	  }
-	}
+	  //	}
 	/* if (SR_selection   && bjet == 0 ){
 	   if (SR_channel == 3 ) Histos2d[0][1][3][fill] -> Fill(D2_delta_pv_sv, M_l2l3_combined,1);
 	   if (SR_channel == 3 ) Histos2d[1][1][3][fill] -> Fill(D2_delta_pv_sv, M_l2l3_combined,1);
